9feaca80ccb78316ec3c4708fa2403c2774ecc1e
nikilandgello <nikoscha22012004@gmail.com>
2024-08-31 14:41:21 +0300

    object

---
+++ js/task-2.js
@@ -0,0 +1,40 @@
+function calcAverageCalories(days) {
+  let sum = 0;
+  const weekLength = 7;
+
+  for (let day of days) {
+    sum += day.calories;
+  };
+
+  let average = sum / weekLength;
+
+  return average;
+};
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+); 
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); 
+
+console.log(
+  calcAverageCalories([])
+); 
